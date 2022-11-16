272c74a1fcf168cb7b0694317b19e9fc69fe51f6
RobertLYucra <robertlyucra@gmail.com>
2022-11-15 19:55:15 -0500

    Ultimo avance

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+const mostrarMenu = document.querySelector('#menu')
+const contenidoMenu = document.querySelector('#options')
+
+
+mostrarMenu.addEventListener('click', () => {
+    mostrarMenu.classList.toggle('animacionMenu')
+    contenidoMenu.classList.toggle('activeMenu')
+
+});
+
+
