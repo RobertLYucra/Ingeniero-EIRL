31fa9a7c0ef86145129d57fd9c55590611515b2d
RobertLYucra <robertlyucra@gmail.com>
2022-11-21 10:09:24 -0500

    ultimo

--- js/script.js
+++ js/script.js
@@ -8,18 +8,3 @@ mostrarMenu.addEventListener('click', () => {
     contenidoMenu.classList.toggle('activeMenu')
 
 });
-
-
-const titulo = document.querySelector('.products');
-let text = "Diseño Estructural - Supervisión - Gestión - Construcción";
-let i = 0;
-
-function writeText(){
-    titulo.innerText = text.slice(0,i);
-    i++;
-    if(i>text.length){
-        i=0;
-    }
-}
-
-setInterval(writeText,100);
